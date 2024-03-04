d1cc4845f22f230bc03ebd3c48851b4a55c0ee22
Borteq2 <borteq@yandex.ru>
2024-03-04 13:11:42 +0200

    refactor timestamp methods (still in dashboard_screen.dart)

--- lib/feature/_dashboard/dashboard_screen.dart
+++ lib/feature/_dashboard/dashboard_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/file.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mordor_suit/feature/_dashboard/widgets/_widgets.dart';
@@ -27,121 +28,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
   bool isNeedLoadData = true;
   String time = '';
 
-  // Future<void> validateTimestamp() async {
-  //   talker.info('1');
-  //   DateTime? cachedTimestamp = await TimestampService.getTimestamp();
-  //   talker.info('2');
-  //   if (cachedTimestamp == null) {
-  //     talker.warning('штамп нул');
-  //     isNeedLoadData = true;
-  //     return;
-  //   }
-  //   talker.info('3');
-  //   talker.warning(cachedTimestamp.difference(DateTime.now()).inMinutes > 3);
-  //   talker.info(cachedTimestamp);
-  //   talker.info(DateTime.now());
-  //   if (cachedTimestamp.difference(DateTime.now()).inMinutes > 3) {
-  //     talker.warning('штамп старый');
-  //     isNeedLoadData = true;
-  //   } else {
-  //     talker.warning('штамп свежий');
-  //     isNeedLoadData = false;
-  //   }
-  //   talker.info('4');
-  //   if (isNeedLoadData) {
-  //     talker.warning('штамп сохраняю дату загружаю');
-  //     await TimestampService.saveTimestamp(DateTime.now());
-  //     try {
-  //       talker.info('5');
-  //       cachedTimestamp = await TimestampService.getTimestamp();
-  //       timestampNow = cachedTimestamp;
-  //     } catch (e, st) {
-  //       talker.critical(e, st);
-  //     }
-  //   } else {
-  //     try {
-  //       talker.info('6');
-  //       talker.warning('штамп только достаю дату не гружу');
-  //       cachedTimestamp = await TimestampService.getTimestamp();
-  //       timestampNow = cachedTimestamp;
-  //     } catch (e, st) {
-  //       talker.critical(e, st);
-  //     }
-  //   }
-  //   talker.info('7');
-  //   setState(() {});
-  // }
-
-  Future<void> checkTimestamp() async {
-    DefaultCacheManager cacheManager = DefaultCacheManager();
-    DateTime now = DateTime.now();
-    int ttlInSeconds = 1800;
-
-    try {
-      var status = await Permission.storage.status;
-      if (status.isDenied || status.isPermanentlyDenied) {
-        status = await Permission.storage.request();
-      }
-      if (status.isGranted) {
-        talker.info('Разрешение на чтение файлов предоставлено');
-      } else {
-        talker.info('Разрешение на чтение файлов не предоставлено');
-      }
-      talker.info('1: Начало проверки таймштампа');
-      FileInfo? timestampFile =
-          await cacheManager.getFileFromCache('timestamp');
-      talker.info('2: Получение таймштампа из кэша завершено');
-      if (timestampFile != null) {
-        String timestampString = await timestampFile.file.readAsString();
-        talker.info('3: Чтение таймштампа из файла завершено');
-        DateTime cachedTimestamp = DateTime.parse(timestampString);
-        if (now.difference(cachedTimestamp).inMinutes > 30) {
-          talker.info('СТАРЫЙ');
-          await cacheManager.putFile(
-            'timestamp',
-            utf8.encode(now.toString()),
-            maxAge: Duration(seconds: ttlInSeconds),
-          );
-          talker.info('УСТАНОВИЛ');
-          talker.info('1: Начало проверки таймштампа');
-          FileInfo? timestampFile =
-              await cacheManager.getFileFromCache('timestamp');
-          talker.info('2: Получение таймштампа из кэша завершено');
-          String timestampString = await timestampFile!.file.readAsString();
-          talker.info('3: Чтение таймштампа из файла завершено');
-          DateTime cachedTimestamp = DateTime.parse(timestampString);
-          time =
-              '${cachedTimestamp.hour}:${cachedTimestamp.minute}:${cachedTimestamp.second}';
-          isNeedLoadData = true;
-        } else {
-          talker.info('СВЕЖИЙ');
-          time =
-              '${cachedTimestamp.hour}:${cachedTimestamp.minute}:${cachedTimestamp.second}';
-          isNeedLoadData = false;
-        }
-      } else {
-        await cacheManager.putFile(
-          'timestamp',
-          utf8.encode(now.toString()),
-          maxAge: Duration(seconds: ttlInSeconds),
-        );
-        talker.info('УСТАНОВИЛ');
-        talker.info('1: Начало проверки таймштампа');
-        FileInfo? timestampFile =
-            await cacheManager.getFileFromCache('timestamp');
-        talker.info('2: Получение таймштампа из кэша завершено');
-        String timestampString = await timestampFile!.file.readAsString();
-        talker.info('3: Чтение таймштампа из файла завершено');
-        DateTime cachedTimestamp = DateTime.parse(timestampString);
-        time =
-            '${cachedTimestamp.hour}:${cachedTimestamp.minute}:${cachedTimestamp.second}';
-      }
-    } catch (e, st) {
-      talker.info('Произошла ошибка при проверке таймштампа:');
-      talker.handle(e, st);
-    }
-  }
-
   @override
   void initState() {
     super.initState();
@@ -152,6 +38,80 @@ class _DashboardScreenState extends State<DashboardScreen> {
     // validateTimestamp();
 
     checkTimestamp().then((_) => setState(() {}));
+  }
+
+  Future<void> checkTimestamp() async {
+    DefaultCacheManager cacheManager = DefaultCacheManager();
+    DateTime currentTime = DateTime.now();
+    int ttlInMinutes = 30;
+
+    _drop(cacheManager);
+
+    try {
+      await _checkStoragePermissions();
+
+      FileInfo? timestampFile = await _getFileFromCache(cacheManager);
+
+      if (timestampFile != null) {
+        String timestampString = await timestampFile.file.readAsString();
+        if (timestampFile.validTill.isBefore(currentTime)) {
+          talker.warning('Таймштамп протух, дропаю кэш');
+          _drop(cacheManager);
+          await _refreshTimestampCache(cacheManager, currentTime, ttlInMinutes);
+        }
+        DateTime cachedTimestamp = DateTime.parse(timestampString);
+        time = '${cachedTimestamp.hour}:${cachedTimestamp.minute}:${cachedTimestamp.second}';
+      } else {
+        await _refreshTimestampCache(cacheManager, currentTime, ttlInMinutes);
+      }
+    } catch (e, st) {
+      talker.info('Произошла ошибка при проверке таймштампа:');
+      talker.handle(e, st);
+    }
+  }
+
+  void _drop(DefaultCacheManager cacheManager) {
+    cacheManager.emptyCache();
+  }
+
+  Future<void> _checkStoragePermissions() async {
+    var status = await Permission.storage.status;
+    if (status.isDenied || status.isPermanentlyDenied) {
+      status = await Permission.storage.request();
+    }
+    if (status.isGranted) {
+      talker.info('Разрешение на чтение файлов предоставлено');
+    } else {
+      talker.info('Разрешение на чтение файлов не предоставлено');
+      throw const PermissionDeniedException('Разрешение на чтение файлов не предоставлено');
+    }
+  }
+
+  Future<void> _refreshTimestampCache(DefaultCacheManager cacheManager, DateTime currentTime, int ttlInMinutes) async {
+    await cacheManager.putFile(
+      'timestamp',
+      utf8.encode(currentTime.toString()),
+      maxAge: Duration(minutes: ttlInMinutes),
+    );
+    talker.info('УСТАНОВИЛ');
+    talker.warning('рефрешу таймштамп');
+    FileInfo? timestampFile = await _getFileFromCache(cacheManager);
+    talker.info('2: Получение таймштампа из кэша завершено');
+
+    if (timestampFile != null) {
+      String timestampString = await timestampFile.file.readAsString();
+      talker.info('3: Чтение таймштампа из файла завершено');
+      DateTime cachedTimestamp = DateTime.parse(timestampString);
+      time =
+      '${cachedTimestamp.hour}:${cachedTimestamp.minute}:${cachedTimestamp.second}';
+      talker.info('ОТРЕФРЕШЕНЫЙ');
+    }
+  }
+
+  Future<FileInfo?> _getFileFromCache(DefaultCacheManager cacheManager) async {
+    FileInfo? timestampFile =
+    await cacheManager.getFileFromCache('timestamp');
+    return timestampFile;
   }
 
   @override
@@ -169,7 +129,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
             ],
           ),
         ),
-        body: Placeholder(),
+        body: const Placeholder(),
         // PresetsGridWidget(appStore: appStore),
         floatingActionButton: Container(
           decoration: BoxDecoration(
@@ -296,3 +256,5 @@ class _AddPresetModalState extends State<AddPresetModal> {
     );
   }
 }
+
+
--- lib/feature/library/logic/timestamp.dart
+++ lib/feature/library/logic/timestamp.dart
@@ -1,38 +1,45 @@
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
-class TimestampService {
-  static const String timestampKey = 'timestamp';
 
 
-  static Future<DateTime?> getTimestamp() async {
-    Talker talker = GetIt.I<Talker>();
-    final Box box = GetIt.I<Box<DateTime>>(instanceName: 'timestamps_box');
 
 
-    final DateTime? stamp = await box.get(0);
-    // talker.info(stamp);
-    // await box.close();
 
-    if (stamp != null) {
-      talker.info('достал штамп $stamp');
-      return stamp;
-    }
-    talker.critical('обосрался, штамп $stamp');
-    throw Exception();
-  }
 
-  static Future<void> saveTimestamp(DateTime timestamp) async {
-    Talker talker = GetIt.I<Talker>();
-      final Box box = GetIt.I<Box<DateTime>>(instanceName: 'timestamps_box');
-
-    // talker.warning(box.keys);
-    talker.warning(box.values);
-
-    talker.info('пытаюсь сохранить штамп $timestamp');
-
-    await box.putAt(0, timestamp);
-    // await box.close();
-  }
-}
+// class TimestampService {
+//   static const String timestampKey = 'timestamp';
+//
+//
+//   static Future<DateTime?> getTimestamp() async {
+//     Talker talker = GetIt.I<Talker>();
+//     final Box box = GetIt.I<Box<DateTime>>(instanceName: 'timestamps_box');
+//
+//
+//     final DateTime? stamp = await box.get(0);
+//     // talker.info(stamp);
+//     // await box.close();
+//
+//     if (stamp != null) {
+//       talker.info('достал штамп $stamp');
+//       return stamp;
+//     }
+//     talker.critical('обосрался, штамп $stamp');
+//     throw Exception();
+//   }
+//
+//   static Future<void> saveTimestamp(DateTime timestamp) async {
+//     Talker talker = GetIt.I<Talker>();
+//       final Box box = GetIt.I<Box<DateTime>>(instanceName: 'timestamps_box');
+//
+//     // talker.warning(box.keys);
+//     talker.warning(box.values);
+//
+//     talker.info('пытаюсь сохранить штамп $timestamp');
+//
+//     await box.putAt(0, timestamp);
+//     // await box.close();
+//   }
+// }
