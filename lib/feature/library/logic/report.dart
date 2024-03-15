fc3b802abbe2b8e6d7d72d2a7b2698d1cda585e2
Borteq2 <borteq@yandex.ru>
2024-03-15 15:10:46 +0200

    add error report

---
+++ lib/feature/library/logic/report.dart
@@ -0,0 +1,27 @@
+import 'package:appmetrica_plugin/appmetrica_plugin.dart';
+import 'package:flutter/foundation.dart';
+
+class Report {
+  static Future<void> map({
+    int? index,
+    required String event,
+    required Map<String, Object> map,
+  }) async =>
+      kReleaseMode ? AppMetrica.reportEventWithMap(event, map) : null;
+
+  static void error({
+    required String message,
+    required String descriptionMessage,
+    required String type,
+  }) =>
+      kReleaseMode
+          ? AppMetrica.reportError(
+              message: message,
+              errorDescription:
+                  AppMetricaErrorDescription.fromCurrentStackTrace(
+                message: descriptionMessage,
+                type: type,
+              ),
+            ).ignore()
+          : null;
+}
