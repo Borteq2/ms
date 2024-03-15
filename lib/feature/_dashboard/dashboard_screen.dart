dcceb1f112edc241b52a085c662546f8c6d1311f
Borteq2 <borteq@yandex.ru>
2024-03-15 15:10:26 +0200

    dashboard reports

--- lib/feature/_dashboard/dashboard_screen.dart
+++ lib/feature/_dashboard/dashboard_screen.dart
@@ -1,5 +1,3 @@
-import 'package:appmetrica_plugin/appmetrica_plugin.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -9,6 +7,7 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 import 'package:mordor_suit/feature/_dashboard/widgets/_widgets.dart';
+import 'package:mordor_suit/feature/library/logic/report.dart';
 import 'package:mordor_suit/feature/library/widgets/_widgets.dart';
 import 'package:mordor_suit/store/_stores.dart';
 
@@ -39,19 +38,18 @@ class _DashboardScreenState extends State<DashboardScreen> {
       appStore.localWeatherStore.localWeatherDataMap = {
         'name': 'Не могу определить местоположение'
       };
-      if (kReleaseMode) {
-        AppMetrica.reportEventWithMap('Не даны разрешения', {
+      Report.map(
+        event: 'Не даны разрешения',
+        map: {
           'Локация': '${permissions[Permission.location]}',
           'Хранилище': '${permissions[Permission.storage]}'
-        });
-        AppMetrica.reportError(
+        },
+      );
+      Report.error(
           message: 'Не даны разрешения',
-          errorDescription: AppMetricaErrorDescription.fromCurrentStackTrace(
-              message: 'Локация: ${permissions[Permission.location]}, '
-                  'Хранилище: ${permissions[Permission.storage]}',
-              type: 'Некорректное взаимодействие с приложеннием'),
-        ).ignore();
-      }
+          descriptionMessage:
+              'Локация: ${permissions[Permission.location]}, Хранилище: ${permissions[Permission.storage]}',
+          type: 'Некорректное взаимодействие с приложеннием');
     }
     appStore.timestampStore.isNeedLoadData
         ? await appStore.weatherPresetsStore.fetchCityWeatherData()
@@ -62,15 +60,14 @@ class _DashboardScreenState extends State<DashboardScreen> {
   void initState() {
     super.initState();
     _requestPermissions();
-    if (kReleaseMode) {
-      AppMetrica.reportEventWithMap(
-        'Открыт экран пресетов погоды',
-        {
-          'Количество пресетов':
-              appStore.weatherPresetsStore.cityNamesStore.presetsCityNamesCount
-        },
-      );
-    }
+
+    Report.map(
+      event: 'Открыт экран пресетов погоды',
+      map: {
+        'Количество пресетов':
+            appStore.weatherPresetsStore.cityNamesStore.presetsCityNamesCount
+      },
+    );
   }
 
   @override
@@ -79,72 +76,83 @@ class _DashboardScreenState extends State<DashboardScreen> {
       builder: (_) => Scaffold(
         appBar: AppBar(title: TitleWidget(appStore: appStore)),
         body: PresetsGridWidget(appStore: appStore),
-        floatingActionButton: Container(
-          decoration: BoxDecoration(
-            color: Colors.black,
-            shape: BoxShape.circle,
-            border: Border.all(
-              color: Theme.of(context).primaryColor,
-              width: 2,
-            ),
-          ),
-          child: PopupMenuButton(
-            icon: SvgPicture.asset(
-              'assets/images/favicon.svg',
-              width: 40,
-              height: 40,
-            ),
-            color: Colors.black.withOpacity(0.9),
-            surfaceTintColor: Colors.transparent,
-            offset: const Offset(-50, -125),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            itemBuilder: (context) {
-              if (kReleaseMode) {
-                AppMetrica.reportEventWithMap(
-                  'Открыто меню FAB',
-                  {
+        floatingActionButton: FABMenuWidget(appStore: appStore),
+        floatingActionButtonLocation: appStore.fabLocation,
+        bottomNavigationBar: BotAppBar(appStore: appStore),
+      ),
+    );
+  }
+}
+
+class FABMenuWidget extends StatelessWidget {
+  const FABMenuWidget({
+    super.key,
+    required this.appStore,
+  });
+
+  final AppStore appStore;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Theme.of(context).primaryColor,
+          width: 2,
+        ),
+      ),
+      child: PopupMenuButton(
+        icon: SvgPicture.asset(
+          'assets/images/favicon.svg',
+          width: 40,
+          height: 40,
+        ),
+        color: Colors.black.withOpacity(0.9),
+        surfaceTintColor: Colors.transparent,
+        offset: const Offset(-50, -125),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        itemBuilder: (context) {
+          Report.map(
+            event: 'Открыто меню FAB',
+            map: {
+              'Локальная погода получена':
+                  appStore.localWeatherStore.isWeatherLoaded &&
+                      !appStore.localWeatherStore.isHasError
+            },
+          );
+
+          return <PopupMenuItem>[
+            PopupMenuItem(
+              value: 1,
+              onTap: () {
+                Report.map(
+                  event: 'Нажата кнопка создания локации по названию города',
+                  map: {
                     'Локальная погода получена':
                         appStore.localWeatherStore.isWeatherLoaded &&
                             !appStore.localWeatherStore.isHasError
                   },
                 );
-              }
-              return <PopupMenuItem>[
-                PopupMenuItem(
-                  value: 1,
-                  onTap: () {
-                    if (kReleaseMode) {
-                      AppMetrica.reportEventWithMap(
-                        'Нажата кнопка создания локации по названию города',
-                        {
-                          'Локальная погода получена':
-                              appStore.localWeatherStore.isWeatherLoaded &&
-                                  !appStore.localWeatherStore.isHasError
-                        },
-                      );
-                    }
-                    showDialog(
-                        context: context,
-                        builder: (context) =>
-                            AddPresetModal(appStore: appStore));
-                  },
-                  child: const Text(
-                    'Добавить город',
-                    style: TextStyle(color: Colors.deepOrange),
-                  ),
-                ),
-                const PopupMenuItem(
-                  value: 2,
-                  child: CustomPopupMenuItem(),
-                ),
-              ];
-            },
-          ),
-        ),
-        floatingActionButtonLocation: appStore.fabLocation,
-        bottomNavigationBar: BotAppBar(appStore: appStore),
+
+                showDialog(
+                    context: context,
+                    builder: (context) => AddPresetModal(appStore: appStore));
+              },
+              child: const Text(
+                'Добавить город',
+                style: TextStyle(color: Colors.deepOrange),
+              ),
+            ),
+            const PopupMenuItem(
+              value: 2,
+              child: CustomPopupMenuItem(),
+            ),
+          ];
+        },
       ),
     );
   }
@@ -175,32 +183,30 @@ class _CustomPopupMenuItemState extends State<CustomPopupMenuItem> {
               ),
         enabled: appStore.localWeatherStore.isWeatherLoaded,
         onTap: () {
-          // TODO: дубликат с тапом по карточке пресета
-          if (kReleaseMode) {
-            AppMetrica.reportEventWithMap(
-              'Нажата кнопка "Одеться здесь и сейчас',
-              {
-                'Локальная погода':
-                    '${appStore.localWeatherStore.temperature}, ${appStore.localWeatherStore.weather}'
-              },
-            );
-          }
+          Report.map(
+            index: null,
+            event: 'Нажата кнопка "Одеться здесь и сейчас',
+            map: {
+              'Локальная погода':
+                  '${appStore.localWeatherStore.temperature}, ${appStore.localWeatherStore.weather}'
+            },
+          );
+
           appStore.currentWeatherStore.setSuitByWeatherManually(
               appStore.localWeatherStore.localWeatherDataMap);
-          if (kReleaseMode) {
-            AppMetrica.reportEventWithMap(
-              'Установлен комплект снаряжения',
-              {
-                'Комплект': appStore.suitStore.suit.name,
-              },
-            );
-            AppMetrica.reportEventWithMap(
-              'Переход на экран комплекта',
-              {
-                'Комплект': appStore.suitStore.suit.name,
-              },
-            );
-          }
+          Report.map(
+            event: 'Установлен комплект снаряжения',
+            map: {
+              'Комплект': appStore.suitStore.suit.name,
+            },
+          );
+          Report.map(
+            event: 'Переход на экран комплекта',
+            map: {
+              'Комплект': appStore.suitStore.suit.name,
+            },
+          );
+
           Navigator.pop(context);
           context.go(
             '/set',
