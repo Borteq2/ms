57d949ae28c2e3de14f4b2b3bd1834910428970d
Borteq2 <borteq@yandex.ru>
2024-03-16 16:38:54 +0200

    refactor sizer_type.dart

---
+++ lib/feature/_item_card/sizer_type.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:mordor_suit/feature/_item_card/_widgets.dart';
+import 'package:mordor_suit/feature/library/logic/report.dart';
+
+class ItemCardSizerTypeWidget extends StatelessWidget {
+  const ItemCardSizerTypeWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.black.withOpacity(0.9),
+      surfaceTintColor: Colors.black,
+      content: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _generateSizerModalWidgetByType(
+              context,
+              type: 'Грудь',
+              buttonText: 'По размерам груди',
+            ),
+            _generateSizerModalWidgetByType(
+              context,
+              type: 'Талия',
+              buttonText: 'По замерам талии',
+            ),
+            _generateSizerModalWidgetByType(
+              context,
+              type: 'Брюки',
+              buttonText: 'По размеру брюк/джинс',
+            ),
+            _generateSizerModalWidgetByType(
+              context,
+              type: 'Рост',
+              buttonText: 'По росту',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  TextButton _generateSizerModalWidgetByType(
+    BuildContext context, {
+    required String type,
+    required String buttonText,
+  }) {
+    return TextButton(
+        onPressed: () {
+          Report.map(
+            event: 'Выбран режим калькуляции размера',
+            map: {
+              'Режим': buttonText,
+            },
+          );
+          Navigator.pop(context);
+          showDialog(
+            context: context,
+            builder: (context) => ItemCardSizeSolutionWidget(
+              sizerType: type,
+            ),
+          );
+        },
+        child: Text(buttonText));
+  }
+}
